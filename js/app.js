bbfd965e966e8d45736a6f910528747f7dccf860
Mena <menae@AZMac-C1MRD5WBG944.local>
2018-06-16 20:35:16 -0700

    new update for triathlete

--- js/app.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-  $('.content').hide();
-  $('#div1').show();
-  $('.project__name').click(function(){
-    $('.content').hide();
-    $('#div'+$(this).attr('target')).show();
-  });
-});
